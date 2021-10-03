2cc24482470c7e41fe618689d4d7268d1a18b588
craftpaperbag <kazu11518@gmail.com>
2021-10-03 18:00:43 +0900

    ファイルパスを第一引数で受け取るようになった

--- sugar_or_salt.py
+++ sugar_or_salt.py
@@ -1,10 +1,24 @@
+import sys
+
 def main():
   version()
+  # ファイル名取得
+  infilepath = get_input_file_path()
+  if infilepath is None:
+    print("ファイルパスを第一引数に与えてください")
+    return
+  print("ファイルパスは" + infilepath + "です")
 
 def version():
   print("-"*12)
   print("sugar or salt")
   print("ver.0.0.0")
   print("-"*12)
+
+def get_input_file_path():
+  args = sys.argv
+  if len(args) < 2:
+    return None
+  return args[1]
 
 main()
